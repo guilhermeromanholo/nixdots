517c9ac0604ba1a8b6cfc6085edf0d2d09d1b61f
guilherme-romanholo <guilhermeromanholobofo@gmail.com>
2025-03-12 00:22:11 -0300

    chore: nix fmt

--- overlays/gruvbox-gtk-theme/default.nix
+++ overlays/gruvbox-gtk-theme/default.nix
@@ -1,15 +1,13 @@
 {prev}:
-prev.gruvbox-gtk-theme.overrideAttrs (oldAttrs:
-  {
-    installPhase = ''
-      runHook preInstall
-  
-      mkdir -p $out/share/themes
-      cd themes
-  
-      ./install.sh -n Gruvbox -t grey --tweaks medium float -d $out/share/themes
-  
-      runHook postInstall
-    '';
-  }
-)
+prev.gruvbox-gtk-theme.overrideAttrs (oldAttrs: {
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/share/themes
+    cd themes
+
+    ./install.sh -n Gruvbox -t grey --tweaks medium float -d $out/share/themes
+
+    runHook postInstall
+  '';
+})
