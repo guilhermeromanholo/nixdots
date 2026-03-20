44b8ccc50720df3589e2f23f3339fce27e9f85c3
guilhermeromanholo <guilhermeromanholobofo@gmail.com>
2026-03-19 23:17:20 -0300

    fix(sddm): fix sddm theme not working

--- modules/services/sddm/default.nix
+++ modules/services/sddm/default.nix
@@ -3,15 +3,19 @@
     services.displayManager.sddm = {
       enable = true;
       wayland.enable = true;
-
       theme = "sddm-astronaut-theme";
-      settings = {Theme = {Current = "sddm-astronaut-theme";};};
 
       extraPackages = with pkgs; [
-        (sddm-astronaut.override {
-          embeddedTheme = "astronaut";
-        })
+        kdePackages.qtsvg
+        kdePackages.qtmultimedia
+        kdePackages.qtvirtualkeyboard
       ];
     };
+
+    environment.systemPackages = with pkgs; [
+      (sddm-astronaut.override {
+        embeddedTheme = "astronaut";
+      })
+    ];
   };
 }
