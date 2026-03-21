fd6963b36a8383478340457dbede87e94c45dd09
guilhermeromanholo <guilhermeromanholobofo@gmail.com>
2026-03-20 23:44:22 -0300

    chore(testvm): reduce hardware.nix

--- modules/hosts/testvm/hardware.nix
+++ modules/hosts/testvm/hardware.nix
@@ -8,10 +8,8 @@
       (modulesPath + "/profiles/qemu-guest.nix")
     ];
 
-    boot.initrd.availableKernelModules = ["ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk"];
-    boot.initrd.kernelModules = [];
     boot.kernelModules = ["kvm-amd"];
-    boot.extraModulePackages = [];
+    boot.initrd.availableKernelModules = ["ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk"];
 
     fileSystems."/" = {
       device = "/dev/disk/by-uuid/544a9352-b2f5-4bd7-ac4b-2ac98a87e3a0";
@@ -23,8 +21,6 @@
       fsType = "vfat";
       options = ["fmask=0022" "dmask=0022"];
     };
-
-    swapDevices = [];
 
     nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   };
