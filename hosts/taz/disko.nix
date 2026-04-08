2bd3585b4267acf7fa1c34204e528f916bd1929d
guilhermeromanholo <guilhermeromanholobofo@gmail.com>
2026-04-08 16:52:03 -0300

    remove boot legacy

--- hosts/taz/disko.nix
+++ hosts/taz/disko.nix
@@ -6,11 +6,6 @@
       content = {
         type = "gpt";
         partitions = {
-          boot = {
-            name = "BOOT";
-            size = "1M";
-            type = "EF02";
-          };
           esp = {
             name = "ESP";
             size = "1G";
