2633a3dc202c23a0a3092d239840b1896922e478
guilhermeromanholo <guilhermeromanholobofo@gmail.com>
2026-04-10 23:40:29 -0300

    chore(ly): add new settings

--- modules/features/services/ly/default.nix
+++ modules/features/services/ly/default.nix
@@ -3,9 +3,15 @@
     services.displayManager.ly = {
       enable = true;
       settings = {
-        xinitrc = null;
-        bigclock = true;
         animation = "matrix";
+        bigclock = "en";
+        bigclock_12hr = true;
+        box_title = "Welcome to NixOS!";
+        hide_version_string = true;
+        text_in_center = true;
+        vi_mode = true;
+        vi_default_mode = "insert";
+        xinitrc = "null";
       };
     };
   };
