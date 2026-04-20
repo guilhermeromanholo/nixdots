216da3bea4d1921262bf93358f50ce62b5ec9e5c
guilhermeromanholo <guilhermeromanholobofo@gmail.com>
2026-04-20 19:02:47 -0300

    chore(hosts): update sunset hardware

--- modules/hosts/sunset/hardware.nix
+++ modules/hosts/sunset/hardware.nix
@@ -1,5 +1,18 @@
 {
-  flake.modules.nixos.sunset = {lib, ...}: {
+  flake.modules.nixos.sunset = {
+    lib,
+    config,
+    modulesPath,
+    ...
+  }: {
+    imports = [
+      (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+    boot.kernelModules = ["kvm-amd"];
+    boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
+
     nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   };
 }
