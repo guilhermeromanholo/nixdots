f1cf1c9082eea8b4ca4918fb6a87388bd613564c
guilhermeromanholo <guilhermeromanholobofo@gmail.com>
2026-04-21 14:53:32 -0300

    chore(niri): update niri keybinds

--- modules/desktop/niri/keybinds.nix
+++ modules/desktop/niri/keybinds.nix
@@ -3,9 +3,24 @@
     config,
     pkgs,
     ...
-  }: {
+  }: let
+    noctalia = cmd: "noctalia-shell ipc call ${cmd}";
+  in  {
     programs.niri.settings.binds = with config.lib.niri.actions; {
+      # Windows
       "super+q".action = close-window;
+      "super+f".action = fullscreen-window;
+      "super+Space".action = toggle-window-floating;
+
+      # Navigation
+      "super+h".action = focus-column-left;
+      "super+l".action = focus-column-right;
+      "super+k".action = focus-workspace-up;
+      "super+j".action = focus-workspace-down;
+
+      # Programs
+      "super+d".action.spawn = noctalia "launcher toggle";
+      "ctrl+alt+l".action.spawn = noctalia "lockScreen lock";
       "super+Return".action = spawn "${pkgs.kitty}/bin/kitty";
     };
   };
