4f4e9e1a2b58a98a9ae092f60465ce8f4bcf6b7c
guilhermeromanholo <guilhermeromanholo@users.noreply.github.com>
2026-04-24 21:58:40 -0300

    chore(niri): update niri input

---
+++ modules/features/desktop/niri/input.nix
@@ -0,0 +1,19 @@
+{
+  flake.modules.homeManager.niri = {
+    programs.niri.settings.input = {
+      keyboard = {
+        xkb.layout = "br";
+      };
+
+      mouse = {
+        enable = true;
+	natural-scroll = false;
+      };
+
+      touchpad = {
+        enable = true;
+	natural-scroll = false;
+      };
+    };
+  };
+}
