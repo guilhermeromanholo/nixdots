e3fb3b8e215b12cba79a5a7bf02702c667d2c602
guilhermeromanholo <guilhermeromanholo@users.noreply.github.com>
2026-09-01 23:58:09 -0300

    chore(flake): replace nixos -> lib

---
+++ modules/flake/lib.nix
@@ -0,0 +1,33 @@
+{
+  inputs,
+  lib,
+  ...
+}: {
+  flake.lib = {
+    # Function used to create a system with the
+    # host module located in modules/hosts.
+    mkNixos = {
+      name,
+      arch,
+      version,
+    }: {
+      ${name} = inputs.nixpkgs.lib.nixosSystem {
+        modules = [
+          inputs.self.modules.nixos.${name}
+          {
+            networking.hostName = name;
+            system.stateVersion = version;
+            nixpkgs.hostPlatform = lib.mkDefault arch;
+          }
+        ];
+      };
+    };
+
+    # Function to check if a group added to a
+    # user exists within the host's context.
+    ifGroupExists = config: groups:
+      builtins.filter
+      (g: builtins.hasAttr g config.users.groups)
+      groups;
+  };
+}
